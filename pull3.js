7d35d3bb43f67719b5a54ba521454efcc127d927
HRITIK DWIVEDI <44141619+hritikrommie@users.noreply.github.com>
2020-10-02 15:35:43 +0530

    added an event listener

--- pull3.js
+++ pull3.js
@@ -1,3 +1,5 @@
 function alertFunction() {
   window.alert("Here is my first JavaScript program,  and my third pull request for Hacktoberfest.  Yay!");
 }
+const btn = document.querySelector('.btn');
+btn.addEventListener('click',alertFunction);
